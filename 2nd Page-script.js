b6ce5e2f041396c12603b9505b9efb54c1549439
Rasadari Dissanayake <155619344+RasadiL99@users.noreply.github.com>
2024-11-30 20:19:04 +0530

    Add files via upload

---
+++ 2nd Page-script.js
@@ -0,0 +1,4 @@
+// Placeholder JavaScript to handle form validation or interactions
+document.querySelector('.sign-in-btn').addEventListener('click', function() {
+    alert('Sign In button clicked!');
+});
---
+++ 3rd Page-script.js
@@ -0,0 +1,5 @@
+// script.js
+document.getElementById("reset-password-form").addEventListener("submit", function (e) {
+    e.preventDefault();
+    alert("Instructions have been sent to your email!");
+});
