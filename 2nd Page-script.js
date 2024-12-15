14678c3fcf82e1b858810a0823607b491150d5b3
Rasadari Dissanayake <155619344+RasadiL99@users.noreply.github.com>
2024-12-15 12:33:10 +0530

    Delete 2nd Page-script.js

--- 2nd Page-script.js
+++
@@ -1,4 +0,0 @@
-// Placeholder JavaScript to handle form validation or interactions
-document.querySelector('.sign-in-btn').addEventListener('click', function() {
-    alert('Sign In button clicked!');
-});
