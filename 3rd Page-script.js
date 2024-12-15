c4ce5cbf54a25785f3ab2c98dd6b6dc93bcafc44
Rasadari Dissanayake <155619344+RasadiL99@users.noreply.github.com>
2024-12-15 12:33:52 +0530

    Delete 3rd Page-script.js

--- 3rd Page-script.js
+++
@@ -1,5 +0,0 @@
-// script.js
-document.getElementById("reset-password-form").addEventListener("submit", function (e) {
-    e.preventDefault();
-    alert("Instructions have been sent to your email!");
-});
